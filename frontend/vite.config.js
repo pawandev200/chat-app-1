d0c0c4577f22c29b307237ba03f412c77183430b
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-12-11 16:22:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'frontend/build', // Set the output directory to 'frontend/build'
+    outDir: 'frontend/dist', // Set the output directory to 'frontend/build'
   },
 })
