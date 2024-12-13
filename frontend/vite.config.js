39315d55ea6e23b7952cb97034ccbf2687f524e7
pawandev <pawandev4691@gmail.com>
2024-12-13 20:47:50 +0530

    Updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'frontend/dist', // Set the output directory to 'frontend/build'
-  },
+  // build: {
+  //   outDir: 'frontend/dist', // Set the output directory to 'frontend/build'
+  // },
 })
